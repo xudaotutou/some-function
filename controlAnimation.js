216e66f125c36e939ba086b98ce7edbb2adbc03b
QAQ <13435638964@163.com>
2022-02-18 01:22:47 +0800

    多个永久的动画同时注册，控制帧率

--- controlAnimation.js
+++ controlAnimation.js
@@ -1,24 +1,42 @@
-export default function animationFrameManager(fps=30,...fns) {
-  const interval = 1000 / fps  // 连续帧之间间隔（理论）
-  let stop = false  // 停止动画
-  let startTime = Date.now()
-  let timeNow = Date.now()  // 当前时间
-  let timeLast = timeNow  // 上一帧时间
-  let delta = 0  // 连续帧之间间隔（实际）
-
-  const step = () => {
-    if (stop) return false
-    timeNow = Date.now()
-    delta = timeNow - timeLast
-    if (timeNow - startTime > 10000) stop = true
-    if (delta > interval) {
-      timeLast = timeNow
-      fns.forEach(fn => {
-        fn()
-      })
+class AnimationFrameManager {
+    constructor(fps = 30, ...fns) {
+        this.interval = 1000 / fps  // 连续帧之间间隔（理论）
+        this.stop = false  // 停止动画
+        this.startTime = Date.now()
+        this.timeNow = Date.now()  // 当前时间
+        this.timeLast = this.timeNow  // 上一帧时间
+        this.delta = 0  // 连续帧之间间隔（实际）
+        this.fns = fns
     }
-    requestAnimationFrame(step)
-  }
 
-  step()
+    run() {
+        this.step()
+        return this
+    }
+
+    end() {
+        this.stop = true
+        return this
+    }
+
+    add(fn) {
+        this.fns.push(fn)
+        return this
+    }
+
+    step() {
+        this.timeNow = Date.now()
+        this.delta = this.timeNow - this.timeLast
+        if (this.delta > this.interval) {
+            this.timeLast = this.timeNow
+            this.fns.forEach(fn => {
+                fn()
+            })
+        }
+        if (!this.stop) requestAnimationFrame(this.step.bind(this))
+    }
+
+
 }
+
+export default  AnimationFrameManager
